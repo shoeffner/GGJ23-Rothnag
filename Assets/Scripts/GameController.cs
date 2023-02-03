c04ef361be594ced4557293d7468bdacbad67280
Sebastian Höffner <info@sebastian-hoeffner.de>
2023-02-03 21:16:16 +0100

    Add Minimal scene and player

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rothnag {
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+}
