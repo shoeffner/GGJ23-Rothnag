b659b3e3382d4bc85192b15c9b8ad1c609f4127b
Sebastian Höffner <info@sebastian-hoeffner.de>
2023-02-04 18:17:37 +0100

    Add TreeNeedsBirdhouse

---
+++ Assets/Scripts/TreeNeedsEvents/TreeNeedsBirdhouseEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Input;
+using UnityEngine.InputSystem;
+
+namespace Rothnag.TreeNeedsEvents
+{
+    public sealed class TreeNeedsBirdhouseEvent : TreeNeedsEvent
+    {
+        private RothnagInputActionAsset.CharacterActionMapActions _instanceCharacterActionMap;
+
+        private void Awake()
+            => _instanceCharacterActionMap = InputProvider.instance.CharacterActionMap;
+
+        private void OnEnable()
+        {
+            _instanceCharacterActionMap.CheatEventProgress.performed += Progress;
+        }
+
+        private void OnDisable()
+        {
+            _instanceCharacterActionMap.CheatEventProgress.performed -= Progress;
+        }
+
+        private void Progress(InputAction.CallbackContext _) => progress += 1;
+    }
+}
