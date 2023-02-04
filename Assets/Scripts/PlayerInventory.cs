c5ebe91b91982ab68429f73c0a193585c07ae255
hartlage <hartlage@uni-bremen.de>
2023-02-04 17:12:50 +0100

    Added PlayerInventory singleton

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace Rothnag
+{
+    public sealed class PlayerInventory : MonoBehaviour
+    {
+    #region Singleton
+
+        private static PlayerInventory _instance;
+
+        public static PlayerInventory instance
+        {
+            get
+            {
+                if (_instance is not null)
+                    return _instance;
+
+                var obj = new GameObject { name = nameof(PlayerInventory) };
+                _instance = obj.AddComponent<PlayerInventory>();
+                DontDestroyOnLoad(obj);
+                return _instance;
+            }
+        }
+
+        private bool EnsureInstantiatedOnlyOnce()
+        {
+            if (_instance is null)
+            {
+                _instance = this;
+                DontDestroyOnLoad(gameObject);
+                return true;
+            }
+
+            Destroy(gameObject);
+            return false;
+        }
+
+    #endregion
+
+        public int bucket;
+        public int animal;
+        public int birdHouse;
+
+        private void Awake()
+        {
+            // for singleton
+            if (!EnsureInstantiatedOnlyOnce())
+                return;
+        }
+    }
+}
