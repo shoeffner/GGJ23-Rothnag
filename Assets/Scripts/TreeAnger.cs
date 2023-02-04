c9ae759cab7129486a87a53c26492acfb2ab44e5
Sebastian Höffner <info@sebastian-hoeffner.de>
2023-02-04 16:02:31 +0100

    Add tree decay

---
+++ Assets/Scripts/TreeAnger.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rothnag {
+
+public class TreeAnger : MonoBehaviour
+{
+    private ScoreKeeper scoreKeeper = null;
+    public SpriteRenderer spriteRenderer = null;
+    public List<Sprite> sprites = new List<Sprite>();
+    private int currentSprite = 0;
+
+    void Awake()
+    {
+        scoreKeeper = GameObject.FindWithTag("ScoreKeeper").GetComponent<ScoreKeeper>();
+        spriteRenderer = spriteRenderer != null ? spriteRenderer : GetComponent<SpriteRenderer>();
+        SetTreeSprite();
+    }
+
+    void Update()
+    {
+        SetTreeSprite();
+    }
+
+    void SetTreeSprite() {
+        if (scoreKeeper.failures != currentSprite) {
+            currentSprite = Mathf.Clamp(scoreKeeper.failures, 0, sprites.Count);
+            spriteRenderer.sprite = sprites[currentSprite];
+        }
+    }
+}
+
+}
