19d5f6753d953d1b6d87982e2de651ecd2ce309d
Andreas Bresser <self@andreasbresser.de>
2023-02-05 13:34:21 +0100

    add colors to bars

--- Assets/Scripts/UI/ItemsPlayerNeeded.cs
+++ Assets/Scripts/UI/ItemsPlayerNeeded.cs
@@ -12,6 +12,10 @@ public sealed class ItemsPlayerNeeded : MonoBehaviour
     public Image animalIndicator;
     public Image bucketIndicator;
 
+    private Color32 red = new Color32(0,255,0,100);
+    private Color32 yellow = new Color32(255,255,0,100);
+    private Color32 green = new Color32(255,0,0,100);
+
     private float testPercentage = 1.0f;
     private TreeNeedsEventManager _treeNeedsEventManager;
 
@@ -46,6 +50,13 @@ public sealed class ItemsPlayerNeeded : MonoBehaviour
 
     private void updateIndicator(Image indicator, float percentage)
     {
+        if (percentage > 0.666) {
+            indicator.GetComponent<Image>().color = red;
+        } else if (percentage > 0.333) {
+            indicator.GetComponent<Image>().color = yellow;
+        } else {
+            indicator.GetComponent<Image>().color = green;
+        }
         indicator.rectTransform.sizeDelta = new Vector2(indicator.rectTransform.sizeDelta.x, percentage * 100);
         indicator.rectTransform.anchoredPosition =
                 new Vector2(indicator.rectTransform.anchoredPosition.x, percentage * 90 + -200);
