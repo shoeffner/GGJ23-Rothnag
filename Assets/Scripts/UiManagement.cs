cb38b8d28ab7fe036a7739db3424d40c75107fbe
Andreas Bresser <self@andreasbresser.de>
2023-02-05 11:41:03 +0100

    create basic UI

---
+++ Assets/Scripts/UiManagement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Rothnag;
+
+public class UiManagement : MonoBehaviour
+{
+    public Image birdhouse;
+    public Image animal;
+    public Image bucket;
+
+    void Update()
+    {
+        // listen to update events would be nicer but this is still a Game Jam
+        birdhouse.enabled = PlayerInventory.instance.birdHouse > 0;
+        animal.enabled = PlayerInventory.instance.animal > 0;
+        bucket.enabled = PlayerInventory.instance.bucket > 0;
+    }
+}
